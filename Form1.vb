3331de1be9212f3bb671a1c62bb0375d0350c633
Ananda NZ <ananda.12@students.amikom.ac.id>
2022-04-04 21:31:05 +0700

    delete anis.php

--- Form1.vb
+++ Form1.vb
@@ -1,9 +1,4 @@
 ﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        '<snippet1>
-        Dim myArray As Array = Array.CreateInstance(GetType(String), 5)
-        '</snippet1>
-    End Sub
-End Class
 
 End Class
+
